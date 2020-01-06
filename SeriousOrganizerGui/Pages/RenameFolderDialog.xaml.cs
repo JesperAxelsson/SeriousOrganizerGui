beaf5b59ce9e43ccbb7188cacd700fe7d354d78d
Jesper Axelsson <jesperaxe@gmail.com>
2020-01-06 14:56:07 +0100

    Enable keyboard commands and add error messages

--- SeriousOrganizerGui/Pages/RenameFolderDialog.xaml.cs
+++ SeriousOrganizerGui/Pages/RenameFolderDialog.xaml.cs
@@ -32,24 +32,75 @@ namespace SeriousOrganizerGui
             txt_new_name.Text = dirEntry.Name;
             txt_new_name.Focus();
         }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            // ... Test for F5 key.
+            if (e.Key == Key.Enter)
+            {
+                Rename();
+            }
+
+            if (e.Key == Key.Escape)
+            {
+                Rename();
+            }
+        }
 
         private void BtnRename_Click(object sender, RoutedEventArgs e)
+        {
+            Rename();
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Cancel();
+        }
+
+        private static bool ContainsInvalidPathChars(string path)
+        {
+            return path.Any(c => Path.GetInvalidPathChars().Contains(c));
+        }
+
+        private static bool ContainsInvalidFileNameChars(string path)
+        {
+            return path.Any(c => Path.GetInvalidFileNameChars().Contains(c));
+        }
+
+        private static bool PathIsValid(string path)
+        {
+            return !ContainsInvalidFileNameChars(path) && !ContainsInvalidPathChars(path);
+        }
+
+
+
+        public void Rename()
         {
             var name = txt_new_name.Text.Trim();
 
             if (string.IsNullOrWhiteSpace(name))
+            {
+                Error("Name may not be only whitespace!");
                 return;
+            }
 
             if (File.Exists(_dirEntry.Path))
             {
                 var parent = Directory.GetParent(_dirEntry.Path);
                 var newPath = Path.Combine(parent.FullName, name);
 
-                if (name.Any(c => Path.GetInvalidFileNameChars().Contains(c)) || File.Exists(newPath))
+                if (!PathIsValid(name))
                 {
                     //Error path already exists!
+                    Error("Filename contains invalid characters!");
                     return;
                 }
+
+                if (File.Exists(newPath))
+                {
+                    Error("File already exists!");
+                    return;
+                }
+
 
                 File.Move(_dirEntry.Path, newPath);
             }
@@ -58,9 +109,16 @@ namespace SeriousOrganizerGui
                 var parent = Directory.GetParent(_dirEntry.Path);
                 var newPath = Path.Combine(parent.FullName, name);
 
-                if (name.Any(c => Path.GetInvalidPathChars().Contains(c)) || Directory.Exists(newPath))
+                if (!PathIsValid(name))
                 {
                     //Error path already exists!
+                    Error("Path contains invalid characters");
+                    return;
+                }
+
+                if (Directory.Exists(newPath))
+                {
+                    Error("Direcory already exists!");
                     return;
                 }
 
@@ -69,14 +127,21 @@ namespace SeriousOrganizerGui
             else
             {
                 // Path does not exist!
+                Error("No file with this is name is found!");
             }
 
             this.Close();
         }
 
-
-        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        private void Cancel()
         {
+            this.Close();
+        }
+
+        private void Error(string errorMessage)
+        {
+            MessageBox.Show(errorMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
             this.Close();
         }
     }
